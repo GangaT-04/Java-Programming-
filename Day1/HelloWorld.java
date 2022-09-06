c70f70bd1b5918556ec3ce65603b599f237807db
Ganga Thangavelu <99279551+GangaT-04@users.noreply.github.com>
2022-09-06 23:31:12 +0530

    Create Day1 Program

---
+++ Day1/HelloWorld.java
@@ -0,0 +1,20 @@
+import java.io.*;
+ class HelloWorld 
+{
+
+    public static void main(String[] args) 
+    {
+      try
+      {
+        DataInputStream d = new DataInputStream(System.in);
+        System.out.print("Enter a number: ");
+        int number =Integer.parseInt(d.readLine());
+      
+        System.out.println("You entered: " + number);
+      }
+      catch(Exception e)
+      {
+        System.out.println(e);
+      }
+    }
+}
