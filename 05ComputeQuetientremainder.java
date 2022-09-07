8b4a720a6477436508e853f007ccea39e1f7d308
Ganga Thangavelu <99279551+GangaT-04@users.noreply.github.com>
2022-09-07 22:27:47 +0530

    Day2 Program

---
+++ 05ComputeQuetientremainder.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class ComputeQuetientremainder
+{
+    public static void main(String args[])
+    {
+        try
+        {
+            DataInputStream d=new DataInputStream(System.in);
+            System.out.println("Enter the Numerator");
+            int num=Integer.parseInt(d.readLine());
+            System.out.println("Enter the Denominator");
+            int deno=Integer.parseInt(d.readLine());
+            int que=num / deno;
+            int rem=num % deno;
+            System.out.println("The Quetient is:"+que);
+            System.out.println("The Remainder is:"+rem);
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
