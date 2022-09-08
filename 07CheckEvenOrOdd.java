95760b1de031ea811d7ee4d68ccaea5382ae5a78
Ganga Thangavelu <99279551+GangaT-04@users.noreply.github.com>
2022-09-08 06:34:40 +0530

    Day3 program

---
+++ 07CheckEvenOrOdd.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class CheckEvenOrOdd
+{
+	public static void main(String[] args)
+	{
+	    try {
+	        DataInputStream d=new DataInputStream(System.in);
+	        System.out.println("Enter the Number");
+	        int n=Integer.parseInt(d.readLine());
+	        if(n%2==0)
+	        {
+	            System.out.println("The number "+n+" is EVEN");
+	        }
+	        else
+	        {
+	            System.out.println("The number "+n+" is ODD");
+	        }
+	        
+	    }
+	    catch(Exception e) {
+	    
+		System.out.println(e);
+	    }
+	}
+}
