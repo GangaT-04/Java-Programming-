fbe74513b36664163913494e0f703e7f8701872f
Ganga Thangavelu <99279551+GangaT-04@users.noreply.github.com>
2022-09-09 06:25:27 +0530

    Day4 Program

---
+++ FactorialNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FactorialNumber
+{
+	public static void main(String[] args) {
+	    
+	    Scanner s= new Scanner(System.in);
+	    
+	    System.out.println("Enter the Number:");
+	    int N=s.nextInt();
+	    int fact=1;
+	    
+	    for(int i=1;i<=N;i++)
+	    {
+	        fact=fact*i;
+	    }
+	    System.out.println("The Sum of Natural Number is "+fact);
+	}
+}
