9642624765adc4e10fea7c156f8f0d2becf55715
Ganga Thangavelu <99279551+GangaT-04@users.noreply.github.com>
2022-09-10 20:34:59 +0530

    Day4 Program

---
+++ 17 FibaonnaciSeries.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String args[])  
+    {    
+        int n1=0,n2=1,n3;    
+    
+        Scanner s=new Scanner(System.in);
+        
+        System.out.print("Enter the Number:");
+        int num=s.nextInt();
+        System.out.print("The Fibonacci Series is "+n1+" "+n2);
+        for(int i=2;i<num;++i)   
+        {    
+           n3=n1+n2;
+           System.out.print(" "+n3); 
+           n1=n2;    
+           n2=n3;    
+        }       
+    }  
+}  
