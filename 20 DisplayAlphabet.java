2390457c1dbfcffe733fa0eb25b1166eb712b405
Ganga Thangavelu <99279551+GangaT-04@users.noreply.github.com>
2022-09-11 08:36:57 +0530

    Day6 Program

---
+++ 20 DisplayAlphabet.java
@@ -0,0 +1,11 @@
+public class DisplayAlphabet
+{  
+    public static void main(String args[])  
+    {  
+        char ch;
+        for(ch='A';ch<='Z';ch++)
+        {
+            System.out.print(" "+ch);
+        }
+    }  
+}  
