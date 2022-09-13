28d53b2ea8882ea808e5b5d065ec40c550118db8
Ganga Thangavelu <99279551+GangaT-04@users.noreply.github.com>
2022-09-13 05:46:14 +0530

    Day8 Program

--- 25 CheckPrimeNumber.java
+++ 25 CheckPrimeNumber.java
@@ -15,14 +15,15 @@ public class CheckPrimeNumber
 	    {
 	        System.out.println(num+" is not a Prime Number");
 	    }
-	    for(int i=2;i<num/2;i++)
+	   for(int i=2;i<=num/2;i++)
 	    {
 	        if(num%i==0)
 	        {
 	            k=1;
+	            break;
 	        }
 	    }
-	    if(k==1)
+	    if(k==0)
 	    {
 	        System.out.println(num+" is a Prime Number");
 	    }
