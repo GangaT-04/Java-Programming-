8e8571431d3569babccea73419fa9bfc529660ca
Ganga Thangavelu <99279551+GangaT-04@users.noreply.github.com>
2022-09-13 05:41:44 +0530

    Day8 Program

---
+++ 25 CheckPrimeNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CheckPrimeNumber
+{
+	public static void main(String[] args) {
+	    
+	    Scanner s= new Scanner(System.in);
+	    
+	    System.out.println("Enter the Number");
+	    int num=s.nextInt();
+	    
+	    int k=0;
+	    
+	    if(num==0 || num==1)
+	    {
+	        System.out.println(num+" is not a Prime Number");
+	    }
+	    for(int i=2;i<num/2;i++)
+	    {
+	        if(num%i==0)
+	        {
+	            k=1;
+	        }
+	    }
+	    if(k==1)
+	    {
+	        System.out.println(num+" is a Prime Number");
+	    }
+	    else{
+	        System.out.println(num+" is not a Prime Number");
+	    }
+	  
+	}
+}
