104c0018f0a6d8ac3bd5c500e60291879e677d96
GangaT-04 <gangathangavelu04@gmail.com>
2022-09-15 17:39:14 +0530

    Day9 program

--- 32 SimpleCalculator.java
+++ 32 SimpleCalculator.java
@@ -4,16 +4,17 @@ public class SimpleCalculator {
     public static void main(String args[])
     {
         Scanner s= new Scanner(System.in);
-        
+        int mention;
+        do
+        {
+        System.out.println("Enter two numbers");
         int a=s.nextInt();
         int b=s.nextInt();
-        int c,mention;
+        int c;
         System.out.println("Simple calculator:");
         System.out.println("1.Addition\n 2.Subtraction\n 3.Multiplication\n 4.Division");
         System.out.println("Enter your calculation");
-        int choice=s.nextInt();
-        do
-        {
+        int choice=s.nextInt();    
         switch(choice)
         {
             case 1:c=a+b;
