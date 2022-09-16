127f792722eec310f21dab8c173e872e3ab273c9
GangaT-04 <gangathangavelu04@gmail.com>
2022-09-16 06:24:34 +0530

    Day10 Program

---
+++ 35 FactorialUsingRecursion.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Main
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter the number:");
+		int number=s.nextInt();
+
+		int fact=factorialOfNumber(number);
+		System.out.print("Factorial of "+number+":"+fact);
+	}
+	static int factorialOfNumber(int number)
+	{
+		if(number!=0)
+		{
+			return number+factorialOfNumber(number-1);
+		}
+		else{
+			return number;
+		}
+	}
+	
+}
