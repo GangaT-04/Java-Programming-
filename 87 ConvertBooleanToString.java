0da0c908acaec242e7cff658b91e56a92b42395f
GangaT-04 <gangathangavelu04@gmail.com>
2022-10-06 21:22:07 +0530

    Day27 Program

---
+++ 87 ConvertBooleanToString.java
@@ -0,0 +1,13 @@
+class ConvertBooleantToString{
+    public static void main(String args[])
+    {
+        boolean b1 = true;
+        boolean b2 = false;
+
+    String s1 = Boolean.toString(b1);
+    String s2 = Boolean.toString(b2);
+
+    System.out.println(s1);    // true
+    System.out.println(s2);    // true
+    }
+}
---
+++ 88 ConvertStringToBoolean.java
@@ -0,0 +1,14 @@
+
+class ConvertStringToBoolean {
+    public static void main(String args[])
+    {
+        String str1 = "true";
+        String str2 = "false";
+
+    
+        boolean b1 = Boolean.valueOf(str1);
+        boolean b2 = Boolean.valueOf(str2);
+    System.out.println(b1);  
+    System.out.println(b2);
+    }
+}
