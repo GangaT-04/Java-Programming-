9537962cb2be9e81e905fb0640ee96b1f4573e33
GangaT-04 <gangathangavelu04@gmail.com>
2022-10-08 17:22:34 +0530

    Day30 Program

---
+++ 93 ConvertStringIntoDouble.java
@@ -0,0 +1,14 @@
+
+class ConvertStringIntoDouble {
+    public static void main(String a[])
+    {
+        String s1="21.983";
+        String s2="432.98364";
+        
+        double d1=Double.parseDouble(s1);
+        double d2=Double.parseDouble(s2);
+        
+        System.out.println(d1);
+        System.out.println(d2);
+    }
+}
---
+++ 94 ConvertDoubleIntoString.java
@@ -0,0 +1,13 @@
+ class ConvertDoubleIntoString {
+    public static void main(String a[])
+    {
+        double d1=98.657;
+        double d2=548.855446;
+        
+        String s1=Double.toString(d1);
+        String s2=Double.toString(d2);
+        
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
